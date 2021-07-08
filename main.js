f4f381ff727525dd04dbbea6cd5695aed9f32861
vaishnavipardeshi2244 <65117680+vaishnavipardeshi2244@users.noreply.github.com>
2021-07-08 19:36:07 +0530

    Add files via upload
    Day 1 Java Script assignment submitted

---
+++ main.js
@@ -0,0 +1,16 @@
+/******************************************************************************
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+let n=5;
+let string="";
+for(let i=1; i<=n; i++){
+    for(let j=0; j<n; j++){
+        string+="*";
+    }
+}
+print(string);
+
